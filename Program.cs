58895d96817173c51a76d1d3e657a705681722a4
Hunter <WilsHunt604@gotvdsb.ca>
2022-10-04 09:40:30 -0400

    eeee

--- Program.cs
+++ Program.cs
@@ -5,6 +5,37 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            MakeHangman(0);
+            MakeHangman(1);
+            MakeHangman(0);
+            MakeHangman(0);
+            MakeHangman(0);
+        }
+        static public void MakeHangman(int stage)
+        {
+            if (stage == 0)
+            {
+                Console.WriteLine("  +---+\r\n  |   |\r\n      |\r\n      |\r\n      |\r\n      |\r\n=========");
+            }
+            else if (stage == 1)
+            {
+                Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n      |\r\n      |\r\n      |\r\n=========");
+            }
+            else if (stage == 2)
+            {
+                Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n /|   |\r\n      |\r\n      |\r\n=========");
+            }
+            else if (stage == 3)
+            {
+                Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n / \\  |\r\n      |\r\n=========");
+            }
+            else if (stage == 4)
+            {
+                Console.WriteLine("  +---+\r\n      |\r\n      | \r\n \\O/  |\r\n  |   |\r\n / \\  |\r\n=========");
+            }
+            else {
+                Console.WriteLine("err");
+            }
         }
     }
 }
